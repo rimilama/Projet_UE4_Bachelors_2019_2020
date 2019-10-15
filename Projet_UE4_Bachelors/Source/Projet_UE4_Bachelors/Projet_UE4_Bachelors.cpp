9cb858f98dc23fff0f0f16375bad1ba0f40e8be4
DESKTOP-NPLCT3S\Antoine <antoinedaeffler@gmail.com>
2019-10-15 08:55:57 +0200

    Introduction Unreal Engine 4 (4.22.3) Création d’un git et commit (push) du projet sur Github Identification des fichiers de compilations du projet UE4

---
+++ Projet_UE4_Bachelors/Source/Projet_UE4_Bachelors/Projet_UE4_Bachelors.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Projet_UE4_Bachelors.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Projet_UE4_Bachelors, "Projet_UE4_Bachelors" );
---
+++ Projet_UE4_Bachelors/Source/Projet_UE4_Bachelors/Projet_UE4_BachelorsGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Projet_UE4_BachelorsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJET_UE4_BACHELORS_API AProjet_UE4_BachelorsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
